e6b59fa729d783b9704cd692f7d870a23cae9515
UBaHbI4 <kinoshnik_ivan@mail.ru>
2021-03-06 15:16:25 +0700

    Создание проекта Calculator

---
+++ app/src/main/java/softing/ubah4ukdev/avers/calculator/CalculatorActivity.java
@@ -0,0 +1,14 @@
+package softing.ubah4ukdev.avers.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
