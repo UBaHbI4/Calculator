304217afcb520d143262545e7a96b962eb2c9235
UBaHbI4 <kinoshnik_ivan@mail.ru>
2021-03-06 22:44:07 +0700

    Выполнена домашняя работа №2 по курсу "Android 1. Знакомство с платформой". В качестве макета выбрал Constraintlayout, т.к. это молодежный, модный, современный :), это отдельная библиотека и поэтому мы получаем его обновления независимо от SDK и Android Studio, что очень удобно. А так же является супер производительным, эффективным и просто удобным, при компоновке компонентов в нем. Все числовые значение, строковые постарался засунуть в ресурсы, вроде ничего не пропустил.

--- app/src/main/java/softing/ubah4ukdev/avers/calculator/CalculatorActivity.java
+++ app/src/main/java/softing/ubah4ukdev/avers/calculator/CalculatorActivity.java
@@ -3,12 +3,13 @@ package softing.ubah4ukdev.avers.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class CalculatorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calculator);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_calculator);
     }
 }
